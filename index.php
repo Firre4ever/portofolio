8a3d7313fb89d0a07db8990f9b313e5e818471dd
Firre4ever <firre.heemskerk@ziggo.nl>
2024-06-05 14:52:03 +0200

    foto
    heb een foto toegevoegd maar moet nog een about me toevoegen

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home Page</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
     <div class="container mt-5">
-        <h1 class="text-center">Welcome to my portofolio</h1>
+        <h1 class="text-center">Welcome to my portofolio </h1>
         <div class="row mt-5">
             <div class="col-md-4">
                 <a href="add_project.php" class="btn btn-primary btn-block">Add Project</a>
@@ -20,6 +21,12 @@
                 <a href="login.php" class="btn btn-info btn-block">Login</a>
             </div>
         </div>
+    </div>
+    <style>
+
+    </style>
+    <div class="image-container">
+        <img class="fotogezicht" src="foto gezicht/foto firre gezicht.jpg" alt="">
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
